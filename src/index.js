3038511a38d2e09df7274d47473217edd634d19c
OceaniaVPN <odimokiy228@gmail.com>
2026-08-21 14:18:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const AUTO_UPDATE_CONFIG = {
 // ВТОРАЯ КОНФИГУРАЦИЯ (Только одна ссылка)
 // ==========================================
 const SECONDARY_CONFIG = {
-  targetUrl: "https://okeaniavpn.dimastekolnikov1.workers.dev/sub?token=10da1f96-8798-4a95-a409-a34cfe9cdc1b",
+  targetUrl: "https://okeaniavpn.dimastekolnikov1.workers.dev/sub?token=0fe191f6-7ec7-44ec-aed7-cc6423745ca8",
   targetFilename: "okeania_auto.txt", // Имя файла для этой подписки
   title: "OkeaniaVPN Auto",
   interval: 4,
