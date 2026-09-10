27bfc9ed455a6f659d05471c3a197e5856db6b63
OceaniaVPN <odimokiy228@gmail.com>
2026-09-10 17:24:03 +0300

    Remove mistakenly added bot index module

--- src/bot/index.js
+++
@@ -1,3 +0,0 @@
-export { handleUpdate } from "./handlers.js";
-export { mainMenu, backToMenu, subscriptionMenu } from "./keyboards.js";
-export { handleCommand, handleMessage, handleCallback } from "./commands.js";
